42b39e31c7cd186f64365ec480dc967ad65574d9
Thyyd <roastingblade@gmail.com>
2026-02-17 11:48:48 +0100

    MàJ fichier tâche 1 : Modification of the DocString

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,23 +1,27 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-BasicCache Module
+This module defines the BasicCache class that inherits from
+BaseCaching and implements a simple dictionary-based cache.
 """
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
     """
-    Class BasicCache that inherits from BaseCaching
-    Redefinition of the put method from BaseCaching
-        - This method adds an item in the cache if parameters are correct
-    Redefinition of the get method from BaseCaching
-        - This method returns the value of an item by key.
+    BasicCache class that inherits from BaseCaching and
+    implements a simple caching system without eviction policy.
     """
     def __init__(self):
         super().__init__()
 
     # Redéfinition de la méthode put
     def put(self, key, item):
+        """
+        Add an item to the cache.
+
+        If key or item is None, the method does nothing.
+        Otherwise, it stores the item in the cache dictionary.
+        """
         if key is None or item is None:
             return  # Arrête la fonction en ne faisant rien
         else:
@@ -25,6 +29,11 @@ class BasicCache(BaseCaching):
 
     # Redéfinition de la méthode get
     def get(self, key):
+        """
+        Retrieve an item from the cache by key.
+
+        Returns None if the key is None or does not exist.
+        """
         if key is None or key not in self.cache_data:
             return None
         else:
