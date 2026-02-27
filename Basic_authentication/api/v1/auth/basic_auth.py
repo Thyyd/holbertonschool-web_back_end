2402b96cdbd951590c58d50458f61460559b80ad
Thyyd <roastingblade@gmail.com>
2026-02-27 12:05:42 +0100

    Ajout des fichiers de la task 10 : Création de la méthode user_object_from_credentials

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,7 +8,9 @@ variable AUTH_TYPE is set to "basic_auth".
 """
 
 from api.v1.auth.auth import Auth
+from typing import TypeVar
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -88,10 +90,10 @@ class BasicAuth(Auth):
 
             Parameters:
                 decoded_base64_authorization_header (str): chaîne de type
-                "email:password"
+                "email:pwd"
 
             Returns:
-                tuple: (email, password) si valide, sinon (None, None)
+                tuple: (email, pwd) si valide, sinon (None, None)
         """
         # Si decoded_base64_authorization_header vaut None
         if decoded_base64_authorization_header is None:
@@ -105,6 +107,46 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
 
-        # Récupération du String avant et après le ':' dans email et password
-        email, password = decoded_base64_authorization_header.split(':', 1)
-        return email, password
+        # Récupération du String avant et après le ':' dans email et pwd
+        email, pwd = decoded_base64_authorization_header.split(':', 1)
+        return email, pwd
+
+    # Méthode user_object_from_credentials
+    def user_object_from_credentials(self, user_email: str, user_pwd:
+                                     str) -> TypeVar('User'):
+        """
+        Retourne l'instance User correspondant à l'email et au mot de passe
+        fournis.
+
+        Parameters:
+            - user_email (str): L'email de l'utilisateur à rechercher.
+            - user_pwd (str): Le mot de passe à vérifier pour l'utilisateur
+            trouvé.
+
+        Returns:
+            - User: L'instance de l'utilisateur si un utilisateur avec cet
+            email existe et si le mot de passe fourni est correct.
+            - None: Si user_email ou user_pwd sont None ou ne sont pas des
+            strings, si aucun utilisateur avec cet email n'existe dans la
+            base de données, ou si le mot de passe fourni ne correspond pas
+            à l'utilisateur.
+        """
+        # Si user_email vaut None OU n'est pas un String
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        # Si user_pwd vaut None OU n'est pas un String
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        # Si l'user n'existe pas dans la base de données
+        user_list = User.search({'email': user_email})
+        if not user_list:
+            return None
+
+        # Si le pwd fourni n'est pas correct
+        user = user_list[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
