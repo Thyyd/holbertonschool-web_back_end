8c469faa604dc43caf6f04f36fae2560b0c77653
Thyyd <roastingblade@gmail.com>
2026-02-27 10:57:48 +0100

    Ajout des fichiers de la task 8 : Création de la méthode decode_base64_authorization_header

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,6 +8,7 @@ variable AUTH_TYPE is set to "basic_auth".
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -45,3 +46,22 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[len("Basic "):]
+
+    # Méthode decode_base64_authorization_header
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        # Si base64_authorization_header vaut None
+        if base64_authorization_header is None:
+            return None
+
+        # Si base64_authorization_header n'est pas une string
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+
+            decoded_string = decoded_bytes.decode("utf-8")
+            return decoded_string
+        except Exception:
+            return None
