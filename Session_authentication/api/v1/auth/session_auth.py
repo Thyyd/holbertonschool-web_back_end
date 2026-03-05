ee273a23d666338abd6edc1ad41d1b7cf0db7027
Thyyd <roastingblade@gmail.com>
2026-03-05 12:23:17 +0100

    Ajout des fichiers de la task 2 : Création d'une Session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -6,6 +6,7 @@ This module defines the SessionAuth class that extends the Auth class.
 """
 
 from api.v1.auth.auth import Auth
+import uuid
 # from typing import TypeVar
 # import base64
 # from models.user import User
@@ -15,4 +16,12 @@ class SessionAuth(Auth):
     """
     SessionAuth class that implements Basic HTTP authentication.
     """
-    pass
+    user_id_by_session_id = {}
+
+    # Méthode create_session
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())  # Création d'un session_id random
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
