e912f55f495007e2fbeda0d4b95814f93ac83e3d
Thyyd <roastingblade@gmail.com>
2026-03-05 09:35:27 +0100

    Ajout des fichiers de la task 1 : Création d'une classe SessionAuth qui hérite de la classe Auth

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module api.v1.auth.session_auth
+
+This module defines the SessionAuth class that extends the Auth class.
+"""
+
+from api.v1.auth.auth import Auth
+# from typing import TypeVar
+# import base64
+# from models.user import User
+
+
+class SessionAuth(Auth):
+    """
+    SessionAuth class that implements Basic HTTP authentication.
+    """
+    pass
