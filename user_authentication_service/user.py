5952d2e6114370025a48971c8ad52d9fd65fb297
Thyyd <roastingblade@gmail.com>
2026-03-17 10:28:18 +0100

    MàJ du fichier de la task 0 : Ajout d'une Docstring à la classe

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module that create a SQLAlchemy model named User for a database
+Module that creates a SQLAlchemy model named User for a database
 table named users
 """
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,6 +11,19 @@ Base = declarative_base()
 
 
 class User(Base):
+    """
+    SQLAlchemy model for the users table.
+
+    Represents a user stored in the database and contains
+    authentication-related fields.
+
+    Attributes:
+        id (int): Primary key of the user.
+        email (str): User email address (not nullable).
+        hashed_password (str): Hashed password of the user (not nullable).
+        session_id (str): Session identifier for the user.
+        reset_token (str): Token used for password reset.
+    """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
