210b9eaa6c82032c336cbd3e549d5c936f717991
Thyyd <roastingblade@gmail.com>
2026-03-19 11:46:07 +0100

    Ajout des fichiers de la task 6 : Création d'une fonction GET utilisant l'endpoint /users

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python3
 """ Basic Flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
 def Welcome():
     """Return a welcome message"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    email = request.form.get('email')
+    pwd = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, pwd)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
