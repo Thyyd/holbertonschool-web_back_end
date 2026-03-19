afc706a5c6a4ac15711d208e3b29027619669bf8
Thyyd <roastingblade@gmail.com>
2026-03-19 23:08:18 +0100

    Ajout des fichiers de la task 11 : Création d'une fonction POST utilisant l'endpoint /sessions

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Flask app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -24,6 +24,37 @@ def users():
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """
+    Handle POST /sessions route.
+
+    Retrieves the email and password from the request form data and
+    verifies the user's credentials. If the credentials are invalid,
+    aborts the request with a 401 status code.
+
+    If the credentials are valid, creates a new session for the user,
+    sets the session ID in a cookie named "session_id", and returns
+    a JSON response containing the user's email and a confirmation
+    message.
+    """
+    # Récupération email et password
+    email = request.form.get('email')
+    pwd = request.form.get('password')
+
+    # Vérification que les informations de login sont correctes
+    if not AUTH.valid_login(email, pwd):
+        abort(401)
+
+    # Création d'une session pour l'user
+    session_id = AUTH.create_session(email)
+    # Ajout du cookie
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+
+    return response
 
 
 if __name__ == "__main__":
