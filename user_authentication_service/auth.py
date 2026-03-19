c0c0ee2deb9ae998689e61db7272afe75adcf9ba
Thyyd <roastingblade@gmail.com>
2026-03-19 01:08:26 +0100

    Ajout des fichiers de la task 5 : Ajout de la méthode register_user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,6 +2,9 @@
 """ Auth module
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -16,3 +19,22 @@ def _hash_password(password: str) -> bytes:
     """
     # Hashage du MdP en convertissant password en bytes et en générant un salt
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            user_pwd = _hash_password(password)
+            user = self._db.add_user(email, user_pwd)
+
+            return user
