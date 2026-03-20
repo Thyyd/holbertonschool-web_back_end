a054829033e56009bfc6ac10594bd5ed9e73c39f
Thyyd <roastingblade@gmail.com>
2026-03-20 14:11:13 +0100

    MàJ des fichiers de la task 18 : Utilisation de 2 update_user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -202,8 +202,7 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
             # Hachage du password
             user_pwd = _hash_password(password)
-            self._db.update_user(user.id,
-                                 hashed_password=user_pwd,
-                                 reset_token=None)
+            self._db.update_user(user.id, hashed_password=user_pwd)
+            self._db.update_user(user.id, reset_token=None)
         except NoResultFound:
             raise ValueError
