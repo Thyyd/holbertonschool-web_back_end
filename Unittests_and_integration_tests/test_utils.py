ea52e76f9f7fd81fd21427eb5ff21f1102f4a0b7
Thyyd <roastingblade@gmail.com>
2026-03-25 16:37:01 +0100

    Ajout des fichiers de la task 2 : Tests unitaires de la fonction get_json

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,8 @@
 """Test module"""
 
 import unittest
-from utils import access_nested_map
+from unittest.mock import patch
+from utils import access_nested_map, get_json
 from parameterized import parameterized
 
 
@@ -23,5 +24,25 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"), KeyError)
     ])
     def test_access_nested_map_exception(self, nested_map, path, expected):
+        # Context manager pour inspecter l'erreur levée.
         with self.assertRaises(expected):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Class for the tests of the get_json function
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    # Remplace requests.get par un Mock pour éviter un appel HTTP réel
+    @patch("utils.requests.get")
+    def test_get_json(self, test_url, expected, mock_get):
+        # Définition de ce que mock doit retourner
+        mock_get.return_value.json.return_value = expected
+        response = get_json(test_url)
+        # Vérification que requests.get a été appelé avec les bons paramètres
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(response, expected)
