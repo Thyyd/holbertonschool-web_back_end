0632eebf4504312a224b10386d109e830a74ee00
Thyyd <roastingblade@gmail.com>
2026-03-27 10:01:01 +0100

    Ajout des fichiers de la task 1 : Création du Setup basique Babel

---
+++ i18n/1-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+App Module
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    # Langue et fuseau horaire utilisés par défaut
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# Création de l'application Flask
+app = Flask(__name__)
+
+# Utilisation de la classe par l'application
+app.config.from_object(Config)
+
+# Création de l'objet Babel relié à l'application Flask
+babel = Babel(app)
+
+
+# Création de la route '/'
+@app.route("/")
+def index():
+    """
+    Function that will print the template
+    """
+    return render_template("1-index.html")
+
+
+# Run the Server
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
