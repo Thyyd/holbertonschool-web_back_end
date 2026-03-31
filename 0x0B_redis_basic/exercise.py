de5d156abd33563c9034c0f21c4ff27480fa81a4
Thyyd <roastingblade@gmail.com>
2026-03-31 12:50:21 +0200

    Ajout des fichiers de la task 1 : Création de getters pour récupérer les valeurs de la DB, soit en str, soit en int.

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -12,6 +12,7 @@ class Cache:
     """
     Class Cache for the DB
     """
+    # Constructeur
     def __init__(self):
         """
         Constructor of the class.
@@ -22,6 +23,7 @@ class Cache:
         # Vide la DB
         self._redis.flushdb()
 
+    # Méthode store
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store a value in Redis using a random key.
@@ -36,3 +38,38 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    # Getter
+    def get(self, key: str, fn=None) -> bytes:
+        """
+        Retrieve a value from Redis by key.
+        Optionally apply a conversion function `fn` to the result.
+        Returns None if key does not exist.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        else:
+            return data
+
+    # Méthode get_int
+    def get_int(self, key):
+        """
+        Retrieve a value from Redis and convert it to int.
+        Returns None if key does not exist.
+        """
+        return self.get(key, int)
+
+    # Méthode get_str
+    def get_str(self, key):
+        """
+        Retrieve a value from Redis and convert it to UFT-8 string.
+        Returns None if key does not exist.
+        """
+        data = self.get(key)
+        if data is None:
+            return None
+        data = data.decode("utf-8")
+        return data
