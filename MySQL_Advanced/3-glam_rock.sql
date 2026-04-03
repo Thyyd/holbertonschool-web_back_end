74b268ca6ecc13fb5effdaea429e2814a912f3d8
Thyyd <roastingblade@gmail.com>
2026-04-03 18:00:34 +0200

    MàJ du fichier de la task 3 : Correction du Where avec un LIKE %XXX%

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -3,5 +3,5 @@
 -- Orders the table of Glam Rock style by their lifespan, in descend order
 SELECT band_name, COALESCE(split, 2024) - formed AS lifespan  -- COALESCE renvoie le premier argument qui n'est pas NULL
 FROM metal_bands
-WHERE style = 'Glam rock'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC
