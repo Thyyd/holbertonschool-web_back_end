d53e3b287c18ffe4784e1ae9bf9b35706db22587
Thyyd <roastingblade@gmail.com>
2026-04-15 10:42:20 +0200

    Ajout des fichiers de la task 2 : Création de deux fontions basiques get et set

---
+++ queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,30 @@
+import redis from "redis";
+
+const client = redis.createClient();
+
+client.on("error", (error) => {
+    console.log(`Redis client not connected to the server: ${error.message}`)
+})
+
+client.on("connect", () => {
+    console.log(`Redis client connected to the server`)
+})
+
+// Fonction setNewSchool
+function setNewSchool(schoolName, value)
+{
+    client.set(schoolName, value, redis.print);
+}
+
+// Fonction displaySchoolValue
+function displaySchoolValue(schoolName)
+{
+    client.get(schoolName, function(err, value) {
+        console.log(value);
+    })
+}
+
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
