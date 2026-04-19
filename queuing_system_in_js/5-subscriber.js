ca6c43a0b73a9b0d8a773fc772c8447a4c4c166d
Thyyd <roastingblade@gmail.com>
2026-04-19 18:50:45 +0200

    Ajout des fichiers de la task 5 : Création d'de 2 Redis clients : 1 Subscriber et 1 Publisher

---
+++ queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,22 @@
+import redis from "redis";
+
+const client = redis.createClient();
+
+client.on("error", (error) => {
+    console.log(`Redis client not connected to the server: ${error.message}`)
+})
+
+client.on("connect", () => {
+    console.log(`Redis client connected to the server`)
+    client.subscribe("holberton school channel");
+})
+
+client.on("message", (channel, message) => {
+    if (message === "KILL_SERVER") {
+        client.unsubscribe(channel);
+        client.quit();
+    }
+    else {
+        console.log(message);
+    }
+})
